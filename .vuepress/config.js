76855405d8f8a9aa894d4b9039068719697c5e7f
Janik von Rotz <login@janikvonrotz.ch>
2020-11-22 21:09:12 +0100

    auto sidebar

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,6 +2,9 @@
 module.exports = {
     title: 'Knowledge Mint System',
     description: 'Knowledge Mint System',
+    themeConfig: {
+        sidebar: 'auto'
+    },
     plugins: [
         'vuepress-plugin-mermaidjs',
         'plausible-analytics',
